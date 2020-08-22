b7a1c9fa2e02aa2276e6272ad6b4ee036de6ddcb
Fayfer Kirill <sayfer555@gmail.com>
2020-08-22 13:21:10 +0600

    Initial Commit

---
+++ coloredLabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coloredLabel
+//
+//  Created by Кирилл Файфер on 22.08.2020.
+//  Copyright © 2020 Кирилл Файфер. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
