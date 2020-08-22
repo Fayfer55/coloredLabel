8169f0cec4f7ef11df125b0786a8d2f57613650e
Fayfer Kirill <sayfer555@gmail.com>
2020-08-22 16:18:53 +0600

    my final version

--- coloredLabel/ViewController.swift
+++ coloredLabel/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var coloredView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slidersChange()
+        viewChange()
     }
 
-
+    @IBAction func redValueChange() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func greenValueChange() {
+         greenValue.text = String(format: "%.2f", greenSlider.value)
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func blueValueChange() {
+         blueValue.text = String(format: "%.2f", blueSlider.value)
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+      private func slidersChange () {
+          redSlider.minimumTrackTintColor = .red
+          greenSlider.minimumTrackTintColor = .green
+          blueSlider.minimumTrackTintColor = .cyan
+      }
+      private func viewChange () {
+          coloredView.layer.cornerRadius = 15
+      }
 }
+
 
